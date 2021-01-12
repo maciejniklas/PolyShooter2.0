88cbeae67083e0b5cacf090df7f5e3036cec04c9
Maciej Niklas <kontakt@mniklas.pl>
2021-01-12 10:51:25 +0100

    Unable to log with empty username

---
+++ Assets/Scripts/UI/PlayButtonInteractable.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    /// <summary>
+    /// Controls button ability to interaction
+    /// </summary>
+    [RequireComponent(typeof(Button))]
+    public class PlayButtonInteractable : MonoBehaviour
+    {
+        private Button _button;
+
+        private void Awake()
+        {
+            // Reference
+            _button = GetComponent<Button>();
+        }
+
+        private void Start()
+        {
+            UsernameInputField.Instance.OnUsernameValidation += ButtonInteractable;
+        }
+
+        private void ButtonInteractable(bool isValid)
+        {
+            _button.interactable = isValid;
+        }
+    }
+}
--- Assets/Scripts/UI/UsernameInputField.cs
+++ Assets/Scripts/UI/UsernameInputField.cs
@@ -10,36 +10,59 @@ namespace UI
     [RequireComponent(typeof(InputField))]
     public class UsernameInputField : MonoBehaviour
     {
-        public static readonly string UsernamePrefKey = "PhotonUsername";
+        public event OnUsernameValidationEventHandler OnUsernameValidation;
+        public delegate void OnUsernameValidationEventHandler(bool isValid);
+        
+        public static UsernameInputField Instance { get; private set; }
 
         private InputField _inputField;
+        
+        private const string UsernamePrefKey = "PhotonUsername";
 
         private void Awake()
         {
+            // Single scene singleton
+            if (Instance != null)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                Instance = this;
+            }
+            
+            // Reference
             _inputField = GetComponent<InputField>();
         }
 
         private void Start()
         {
-            var defaultName = string.Empty;
-
-            if (PlayerPrefs.HasKey(UsernamePrefKey))
-            {
-                defaultName = PlayerPrefs.GetString(UsernamePrefKey);
-                _inputField.text = defaultName;
-            }
-
+            // Initialize text field of input field
+            if (!PlayerPrefs.HasKey(UsernamePrefKey)) return;
+            var defaultName = PlayerPrefs.GetString(UsernamePrefKey);
+            _inputField.text = defaultName;
             PhotonNetwork.NickName = defaultName;
+            OnUsernameValidation?.Invoke(true);
+        }
+
+        private void OnDestroy()
+        {
+            if (Instance == this)
+            {
+                Instance = null;
+            }
         }
 
         public void UpdatePlayerUsername(string username)
         {
             if (string.IsNullOrWhiteSpace(username))
             {
+                OnUsernameValidation?.Invoke(false);
                 Notification.Instance.ErrorMessage("Enter valid username!");
                 return;
             }
 
+            OnUsernameValidation?.Invoke(true);
             PhotonNetwork.NickName = username;
             PlayerPrefs.SetString(UsernamePrefKey, username);
         }
