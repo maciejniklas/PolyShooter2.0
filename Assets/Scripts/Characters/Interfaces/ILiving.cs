f464eedc885f9b574ce43bc900f84b198157f7e4
Maciej Niklas <kontakt@mniklas.pl>
2021-01-12 20:02:20 +0100

    ILiving interface

---
+++ Assets/Scripts/Characters/Interfaces/ILiving.cs
@@ -0,0 +1,28 @@
+﻿namespace Characters.Interfaces
+{
+    /// <summary>
+    /// Implements behaviour of living creature
+    /// </summary>
+    public interface ILiving
+    {
+        float Health { get; }
+        float HealthRegenerationPerSecond { get; }
+        float MaxHealth { get; }
+        float MaxStamina { get; }
+        float Stamina { get; }
+        float StaminaRegenerationPerSecond { get; }
+
+        void Death();
+        void Hurt(float damage);
+        void Tire(float effortCost);
+        void Regeneration();
+
+        event OnDeathEventHandler OnDeath;
+        event OnHealthValueChangedEventHandler OnHealthValueChanged;
+        event OnStaminaValueChangedEventHandler OnStaminaValueChanged;
+    }
+    
+    public delegate void OnDeathEventHandler();
+    public delegate void OnHealthValueChangedEventHandler(float health);
+    public delegate void OnStaminaValueChangedEventHandler(float stamina);
+}
