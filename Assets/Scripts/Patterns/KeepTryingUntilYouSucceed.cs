03afbc01856cc3b9fc5a654b97440d1b69d3aab4
Maciej Niklas <kontakt@mniklas.pl>
2021-01-12 21:56:21 +0100

    KeepTryingUntilYouSucceed pattern

---
+++ Assets/Scripts/Patterns/KeepTryingUntilYouSucceed.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Patterns
+{
+    public class KeepTryingUntilYouSucceed
+    {
+        private readonly float _interval;
+        private readonly Action _callback;
+
+        public KeepTryingUntilYouSucceed(float interval, Action callback)
+        {
+            _interval = interval;
+            _callback = callback;
+        }
+
+        public IEnumerator Try()
+        {
+            var result = false;
+
+            while (!result)
+            {
+                yield return new WaitForSeconds(_interval);
+                
+                try
+                {
+                    _callback();
+                    result = true;
+                }
+                catch (Exception)
+                {
+                    // ignored
+                }
+            }
+        }
+    }
+}
