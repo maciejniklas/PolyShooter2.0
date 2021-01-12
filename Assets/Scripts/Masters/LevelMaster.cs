f62ab6a6e5c1be24a56bb48d20fe5559b2336ec4
Maciej Niklas <kontakt@mniklas.pl>
2021-01-12 12:57:40 +0100

    Spawn player

---
+++ Assets/Scripts/Masters/LevelMaster.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Masters
+{
+    public class LevelMaster : MonoBehaviour
+    {
+        [Tooltip("Used for right receiving all buffered events, RPC, etc.")]
+        [SerializeField] private float spawnWaitTimeInSeconds = 1f;
+        [SerializeField] private List<GameObject> playerPrefabsPool;
+        [SerializeField] private List<Transform> spawnPointsPool;
+
+        private const byte SynchronizeSpawnPointsEventCode = 1;
+
+        private void Start()
+        {
+            StartCoroutine(SpawnPlayer());
+        }
+
+        private IEnumerator SpawnPlayer()
+        {
+            // Give short time to receive all buffered events, RPC, etc.
+            yield return new WaitForSeconds(spawnWaitTimeInSeconds);
+
+            int randomSpawnPointIndex = Random.Range(0, spawnPointsPool.Count);
+            Transform spawnPoint = spawnPointsPool[randomSpawnPointIndex];
+
+            int randomPlayerPrefabIndex = Random.Range(0, playerPrefabsPool.Count);
+            GameObject randomPlayerPrefab = playerPrefabsPool[randomPlayerPrefabIndex];
+
+            GameObject playerInstance =
+                PhotonNetwork.Instantiate(randomPlayerPrefab.name, spawnPoint.position, spawnPoint.rotation);
+        }
+    }
+}
