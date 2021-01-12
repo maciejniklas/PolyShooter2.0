38a48ee27579e51a7bd5f2a241fef1b6e64c075e
Maciej Niklas <kontakt@mniklas.pl>
2021-01-12 13:26:30 +0100

    FPP camera at local client

---
+++ Assets/Scripts/Characters/Player/PlayerModule.cs
@@ -0,0 +1,26 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+namespace Characters.Player
+{
+    public class PlayerModule : MonoBehaviourPun
+    {
+        [Header("Initialization")]
+        [SerializeField] private GameObject virtualCamera;
+        [SerializeField] private GameObject visualRepresentation;
+
+        private void Start()
+        {
+            if (photonView.IsMine)
+            {
+                InitializeFpp();
+            }
+        }
+
+        private void InitializeFpp()
+        {
+            visualRepresentation.SetActive(false);
+            virtualCamera.SetActive(true);
+        }
+    }
+}
