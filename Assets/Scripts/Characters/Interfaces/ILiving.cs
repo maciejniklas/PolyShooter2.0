fa1e3a165361df65b663b388e9dcd9f8c7f8aea7
Maciej Niklas <kontakt@mniklas.pl>
2021-01-12 21:03:51 +0100

    ILiving upgrades

--- Assets/Scripts/Characters/Interfaces/ILiving.cs
+++ Assets/Scripts/Characters/Interfaces/ILiving.cs
@@ -1,4 +1,6 @@
-﻿namespace Characters.Interfaces
+﻿using System.Collections;
+
+namespace Characters.Interfaces
 {
     /// <summary>
     /// Implements behaviour of living creature
@@ -7,15 +9,24 @@
     {
         float Health { get; }
         float HealthRegenerationPerSecond { get; }
+        bool IsAbleToTire { get; }
+        bool IsAfterActivity { get; }
+        bool IsAlive { get; }
+        bool IsTarget { get; }
         float MaxHealth { get; }
         float MaxStamina { get; }
         float Stamina { get; }
         float StaminaRegenerationPerSecond { get; }
+        float TimeToBeSafeInSeconds { get; }
+        float TimeToRestInSeconds { get; }
 
         void Death();
+        void HealthRegeneration();
         void Hurt(float damage);
+        IEnumerator RestTimer();
+        IEnumerator SafeTimer();
+        void StaminaRegeneration();
         void Tire(float effortCost);
-        void Regeneration();
 
         event OnDeathEventHandler OnDeath;
         event OnHealthValueChangedEventHandler OnHealthValueChanged;
--- Assets/Scripts/Characters/Player/PlayerModule.cs
+++ Assets/Scripts/Characters/Player/PlayerModule.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections;
 using Characters.Interfaces;
 using Photon.Pun;
 using UnityEngine;
@@ -11,18 +11,27 @@ namespace Characters.Player
         [SerializeField] private GameObject virtualCamera;
         [SerializeField] private GameObject visualRepresentation;
 
+        [Header("Living behaviour")]
+        [SerializeField] private float timeToBeSafeInSeconds = 5f;
         [SerializeField] private float healthRegenerationPerSecond = 1f;
         [SerializeField] private float maxHealth = 100f;
+        [SerializeField] private float timeToRestInSeconds = 5f;
         [SerializeField] private float staminaRegenerationPerSecond = 2.5f;
         [SerializeField] private float maxStamina = 100f;
 
         public float Health { get; private set; }
         public float HealthRegenerationPerSecond => healthRegenerationPerSecond;
+        public bool IsAbleToTire => Stamina > 0;
+        public bool IsAfterActivity { get; private set; }
+        public bool IsAlive => Health > 0;
+        public bool IsTarget { get; private set; }
         public float MaxHealth => maxHealth;
         public float MaxStamina => maxStamina;
         public float Stamina { get; private set; }
         public float StaminaRegenerationPerSecond => staminaRegenerationPerSecond;
-        
+        public float TimeToBeSafeInSeconds => timeToBeSafeInSeconds;
+        public float TimeToRestInSeconds => timeToRestInSeconds;
+
         public static PlayerModule LocalPlayer { get; private set; }
 
         public event OnDeathEventHandler OnDeath;
@@ -34,6 +43,8 @@ namespace Characters.Player
             // Parameters initialization
             Health = MaxHealth;
             Stamina = MaxStamina;
+            IsAfterActivity = false;
+            IsTarget = false;
             
             if (!photonView.IsMine) return;
             
@@ -60,9 +71,30 @@ namespace Characters.Player
         {
             if (!photonView.IsMine) return;
 
-            if (Health < MaxHealth || Stamina < MaxStamina)
+            // Editor testing features
+            if (Application.isEditor)
             {
-                Regeneration();
+                if (Input.GetKeyDown(KeyCode.Alpha0))
+                {
+                    Hurt(10);
+                }
+                else if (Input.GetKeyDown(KeyCode.Alpha9))
+                {
+                    Tire(10);
+                }
+            }
+
+            if (!IsAlive) return;
+            
+            // Regeneration
+            if (Health < MaxHealth && !IsTarget)
+            {
+                HealthRegeneration();
+            }
+
+            if (Stamina < MaxStamina && !IsAfterActivity)
+            {
+                StaminaRegeneration();
             }
         }
 
@@ -84,41 +116,83 @@ namespace Characters.Player
             OnDeath?.Invoke();
         }
 
+        public void HealthRegeneration()
+        {
+            Health += HealthRegenerationPerSecond * Time.deltaTime;
+            if (Health > MaxHealth) Health = Mathf.Clamp(Health, 0, MaxHealth);
+            OnHealthValueChanged?.Invoke(Health);
+        }
+
         public void Hurt(float damage)
         {
             if (!photonView.IsMine) return;
 
+            // Update health value
             Health -= damage;
 
+            // Clamp value if over the range
             if (Health < 0) Health = Mathf.Clamp(Health, 0, MaxHealth);
             
+            // Notify listeners
             OnHealthValueChanged?.Invoke(Health);
 
+            // Die if have no health
             if (Health == 0) Death();
+
+            // Update IsTarget flag
+            if (IsTarget)
+            {
+                StopCoroutine(SafeTimer());
+            }
+            else
+            {
+                IsTarget = true;
+            }
+            StartCoroutine(SafeTimer());
+        }
+
+        public IEnumerator RestTimer()
+        {
+            yield return new WaitForSeconds(TimeToRestInSeconds);
+            IsAfterActivity = false;
+        }
+
+        public IEnumerator SafeTimer()
+        {
+            yield return new WaitForSeconds(TimeToBeSafeInSeconds);
+            IsTarget = false;
+        }
+
+        public void StaminaRegeneration()
+        {
+            Stamina += StaminaRegenerationPerSecond * Time.deltaTime;
+            if (Stamina > MaxStamina) Stamina = Mathf.Clamp(Stamina, 0, MaxStamina);
+            OnStaminaValueChanged?.Invoke(Stamina);
         }
 
         public void Tire(float effortCost)
         {
             if (!photonView.IsMine) return;
             
+            // Update stamina value
             Stamina -= effortCost;
 
+            // Clamp value if over the range
             if (Stamina < 0) Stamina = Mathf.Clamp(Stamina, 0, MaxStamina);
             
+            // Notify listeners
             OnStaminaValueChanged?.Invoke(Stamina);
-        }
 
-        public void Regeneration()
-        {
-            if (!photonView.IsMine) return;
-            
-            Health += HealthRegenerationPerSecond * Time.deltaTime;
-            if (Health > MaxHealth) Health = Mathf.Clamp(Health, 0, MaxHealth);
-            OnHealthValueChanged?.Invoke(Health);
-
-            Stamina += StaminaRegenerationPerSecond * Time.deltaTime;
-            if (Stamina > MaxStamina) Stamina = Mathf.Clamp(Stamina, 0, MaxStamina);
-            OnStaminaValueChanged?.Invoke(Stamina);
+            // Update IsAfterActivity flag
+            if (IsAfterActivity)
+            {
+                StopCoroutine(RestTimer());
+            }
+            else
+            {
+                IsAfterActivity = true;
+            }
+            StartCoroutine(RestTimer());
         }
 
         private void InitializeFpp()
