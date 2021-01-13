52bcd87acb3feae28202f711706056af93618fef
Maciej Niklas <kontakt@mniklas.pl>
2021-01-13 16:54:05 +0100

    IWeapon interface

---
+++ Assets/Scripts/Weapons/Interfaces/IWeapon.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Weapons.Interfaces
+{
+    /// <summary>
+    /// Implements weapon behaviour
+    /// </summary>
+    public interface IWeapon
+    {
+        float Damage { get; }
+        GameObject Instance { get; }
+        GameObject Owner { get; }
+
+        void Attack();
+
+        event OnWeaponAttackEventHandler OnWeaponAttack;
+    }
+
+    public delegate void OnWeaponAttackEventHandler();
+}
