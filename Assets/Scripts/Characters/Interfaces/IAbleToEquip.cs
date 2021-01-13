b77f82c4aa4b20a5d5a8eebc47a9a8d35ecfcbc1
Maciej Niklas <kontakt@mniklas.pl>
2021-01-13 20:04:27 +0100

    IAbleToEquip

---
+++ Assets/Scripts/Characters/Interfaces/IAbleToEquip.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Weapons.Interfaces;
+
+namespace Characters.Interfaces
+{
+    /// <summary>
+    /// Implements behaviours that enable to equipping items
+    /// </summary>
+    public interface IAbleToEquip
+    {
+        IWeapon EquippedWeapon { get; }
+        
+        void EquipWeapon(IWeapon weapon);
+    }
+}
