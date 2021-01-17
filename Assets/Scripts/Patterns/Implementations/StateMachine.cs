ee0eae3aeb6f9af06f19091dd6aacdf46317df60
Maciej Niklas <kontakt@mniklas.pl>
2021-01-17 19:32:41 +0100

    State machine implementation for non MonoBehaviour classes

---
+++ Assets/Scripts/Patterns/Implementations/StateMachine.cs
@@ -0,0 +1,27 @@
+﻿using Patterns.Interfaces;
+
+namespace Patterns.Implementations
+{
+    public abstract class StateMachine : IStateMachine
+    {
+        public IState CurrentState { get; private set; }
+        
+        protected StateMachine() {}
+
+        protected StateMachine(IState initialState)
+        {
+            ChangeState(initialState);
+        }
+        
+        public void ChangeState(IState newState)
+        {
+            CurrentState?.DestroyState();
+
+            CurrentState = newState;
+
+            if (CurrentState == null) return;
+            CurrentState.Owner = this;
+            CurrentState.PrepareState();
+        }
+    }
+}
